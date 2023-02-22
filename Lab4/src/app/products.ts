d4365f6f2fc22a9df4cea2b354a39ed37595d1f2
isolent <98767073+isolent@users.noreply.github.com>
2023-02-22 17:52:15 +0600

    lab4

---
+++ Lab4/src/app/products.ts
@@ -0,0 +1,126 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  details: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Smartphone Apple iPhone 14 PRO Max',
+    price: 702990,
+    description: 'A large phone with one of the best screens',
+    details:
+      'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-fioletovyi-106363342/?c=750000000',
+    photo:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h00/h18/62948780834846/apple-iphone-14-pro-max-128gb-fioletovyj-106363342-1.jpg',
+    raiting: 5,
+  },
+  {
+    id: 2,
+    name: 'Laptop Apple MacBook Air Space Gray M2',
+    price: 649990,
+    description: 'The best choice in your life',
+    details:
+      'https://kaspi.kz/shop/p/apple-macbook-air-13-mly33-chernyi-105933768/?c=750000000',
+    photo:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h12/hcd/52679174324254/apple-macbook-air-13-mly33-chernyi-105933768-1.jpg',
+    raiting: 5,
+  },
+  {
+    id: 3,
+    name: 'Smartphone Samsung Galaxy S23+',
+    price: 599990,
+    description: 'A great phone with one of the best cameras',
+    details:
+      'https://kaspi.kz/shop/p/samsung-galaxy-s23-5g-8-gb-512-gb-zelenyi-podarok-108714365/?c=750000000',
+    photo:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h0a/h3e/68538727563294/samsung-galaxy-s23-5g-8-gb-512-gb-zelenyi-108714365-1.jpg',
+    raiting: 5,
+  },
+  {
+    id: 4,
+    name: 'Refrigerator Samsung RT-38K5535EF',
+    price: 324990,
+    description: 'A large and roomy refrigerator',
+    details:
+      'https://kaspi.kz/shop/p/samsung-rt38k5535ef-wt-bezhevyi-100601535/?c=750000000#!/item',
+    photo:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hda/h58/32952853463070/samsung-rt38k5535ef-wt-bezevyj-100601535-1-Container.jpg',
+    raiting: 5,
+  },
+  {
+    id: 5,
+    name: 'Earphone Apple AirPods 2nd Generation',
+    price: 119630,
+    description: 'The music in your mind',
+    details:
+      'https://kaspi.kz/shop/p/apple-airpods-pro-2nd-generation-belyi-106362968/?c=750000000#!/item',
+    photo:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hba/hf8/62281477259294/apple-airpods-pro-2nd-generation-belyj-106362968-1.jpg',
+    raiting: 5,
+  },
+  {
+    id: 6,
+    name: 'Smartwatch Apple Watch SE',
+    price: 167298,
+    description: 'beautiful and helpful watch',
+    details:
+      'https://kaspi.kz/shop/p/apple-watch-se-2nd-gen-40-mm-zolotistyi-106362759/?c=750000000#!/item',
+    photo:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h97/h46/62711073407006/apple-watch-se-2nd-generation-40-mm-zolotistyj-106362759-1.jpg',
+    raiting: 5,
+  },
+  {
+    id: 7,
+    name: 'Cooking Chief KENWOOD',
+    price: 649990,
+    description: 'no need to cook',
+    details:
+      'https://kaspi.kz/shop/p/kenwood-kcl95-004si-cooking-chef-xl-serebristyi-100883789/?c=750000000#!/item',
+    photo:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h00/hc8/33543472480286/kenwood-kcl95-004si-cooking-chef-xl-serebristyj-100883789-1-Container.jpg',
+    raiting: 5,
+  },
+  {
+    id: 8,
+    name: ' TV Samsung UE75AU7100UXCE 191 ',
+    price: 619670,
+    description: 'cinema at home',
+    details:
+      'https://kaspi.kz/shop/p/samsung-ue75au7100uxce-191-sm-chernyi-101496170/?c=750000000#!/item',
+    photo:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hdf/he5/34209576779806/samsung-ue75au7100uxce-190-sm-cernyj-101496170-1-Container.jpg',
+    raiting: 5,
+  },
+  {
+    id: 9,
+    name: 'Game console Sony PlayStation 5',
+    price: 332985,
+    description: 'the great present for boys',
+    details:
+      'https://kaspi.kz/shop/p/sony-playstation-5-belyi-100746577/?c=750000000#!/item',
+    photo:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hca/h70/33035111104542/sony-playstation-5-belyj-100746577-2-Container.jpg',
+    raiting: 5,
+  },
+  {
+    id: 10,
+    name: 'Blender Bosch MSM2650B',
+    price: 26070,
+    description: 'Good blender with high quality',
+    details:
+      'https://kaspi.kz/shop/p/bosch-msm2650b-3100958/?c=750000000#!/item',
+    photo:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h98/hbd/49221585666078/bosch-msm-2650b-black-3100958-1-Container.jpg',
+    raiting: 5,
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
